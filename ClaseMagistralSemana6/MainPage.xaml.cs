4e90151c29245fed663a5f81b381cb59c7bfea74
master <master@SMLAB408PC02_23>
2023-01-17 19:14:27 -0500

    Agregar archivos de proyecto.

---
+++ ClaseMagistralSemana6/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClaseMagistralSemana6
+{
+    public partial class MainPage : ContentPage
+    {
+       // private const string Url = "http://localhost/proyecto/post.php";
+        private const string Url = "http://eyecomers.com/estudiante.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<ClaseMagistralSemana6.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+            obtener();
+        }
+
+        public async void obtener( )
+        {
+            var content = await client.GetStringAsync(Url);
+            List<ClaseMagistralSemana6.Datos> posts = JsonConvert.DeserializeObject<List<ClaseMagistralSemana6.Datos>>(content);
+            _post = new ObservableCollection<ClaseMagistralSemana6.Datos>(posts);
+
+            MyListView.ItemsSource = _post;
+
+        }
+
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
---
+++ ClaseMagistralSemana6/Registro.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClaseMagistralSemana6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtId.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+
+              var res=  client.UploadValues("http://eyecomers.com/estudiante.php", "POST", parametros);
+                DisplayAlert("Operacion realizada", "", "Cerrar");
+
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+     
+        }
+    }
+}
